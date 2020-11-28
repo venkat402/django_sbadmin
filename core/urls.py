f6e18380062ac9671cc6d3f7965d1bcf4b0bbea7
Venkat Adapala <venkat.adapala@ahex.co.in>
2020-11-28 17:14:09 +0530

    Make the pages working

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,19 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index),
-    url(r'home/$',views.home),
+    url(r'test/$',views.test),
+    url(r'error-404/$', views.error_404),
+    url(r'blank/$', views.blank),
+    url(r'buttons/$', views.buttons),
+    url(r'cards/$', views.cards),
+    url(r'charts/$', views.charts),
+    url(r'forgot-password/$', views.forgot_password),
+    url(r'login/$', views.login),
+    url(r'register/$', views.register),
+    url(r'tables/$', views.tables),
+    url(r'u-animation/$', views.u_animation),
+    url(r'u-border/$', views.u_border),
+    url(r'u-color/$', views.u_color),
+    url(r'u-other/$', views.u_other),
 ]
 
--- core/views.py
+++ core/views.py
@@ -2,20 +2,80 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def error_404_view(request, exception):
+def error_404_view(request):
     data = {"name": "ThePythonDjango.com"}
-    return render(request, 'myapp/error_404.html', data)
+    return render(request, '404.html', data)
 
 
-def home(request):
+def index(request):
     data = {"name": "ThePythonDjango.com"}
     return render(request, 'index.html', data)
 
 
-def login(request, exception):
-    data = {"name": "ThePythonDjango.com"}
-    return render(request, 'myapp/error_404.html', data)
-
-
-def index(request):
+def test(request):
     return HttpResponse("Hello, world. You're at the Home page of Django sample project error 404.")
+
+
+def error_404(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, '404.html', data)
+
+
+def blank(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'blank.html', data)
+
+
+def buttons(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'buttons.html', data)
+
+
+def cards(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'cards.html', data)
+
+
+def charts(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'charts.html', data)
+
+
+def forgot_password(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'forgot-password.html', data)
+
+
+def login(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'login.html', data)
+
+
+def register(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'register.html', data)
+
+
+def tables(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'tables.html', data)
+
+
+def u_animation(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'utilities-animation.html', data)
+
+
+def u_border(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'utilities-border.html', data)
+
+
+def u_color(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'utilities-color.html', data)
+
+
+def u_other(request):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request, 'utilities-other.html', data)
